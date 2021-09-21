c94166d8c9f358cbc5d521562d584e42b68b5362
Celtuzed <uter222222@gmail.com>
2021-09-21 19:24:21 +0300

    Renamed the file and removed the function

--- download_image.py
+++ download_image.py
@@ -8,10 +8,3 @@ def download_image(link, path, params=None):
 
     with open(path, "wb") as file:
         file.write(response.content)
-
-
-def check_status(response):
-    if response.raise_for_status():
-        raise requests.HTTPError
-    elif "error" in response.json():
-        raise requests.exceptions.HTTPError(response.json()['error'])
